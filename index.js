90200cf02ee002df45a367bb3e009c5cb820c83a
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-12-19 15:02:36 +0530

    M-2QQ4

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const yt = await Innertube.create({
   device_category: "desktop",
   cache: new UniversalCache(false),
   generate_session_locally: true,
-  cookie:"_gcl_au=1.1.1502659297.1728791448;VISITOR_INFO1_LIVE=NvNH48Rg2dE;VISITOR_PRIVACY_METADATA=CgJJThIEGgAgJA%3D%3D;PREF=repeat=NONE&volume=100&guide_collapsed=false;SID=g.a000rQjsn32GTZix0H63r-03LdqfckVowcTJ8U6_JUT1fYHlxRXizf8YjdglJfdDyAUTld-bNwACgYKAY4SARQSFQHGX2Mi04b-rfj4nxoOMKhqXSruPBoVAUF8yKq091uA0aDNjSCrhqgSMvtq0076;__Secure-1PSIDTS=sidts-CjIB7wV3saV3hV8wjb6dGFtm_sPA85jqqygakc5JiaOhtUVOpOdvui_cbgfIVYxsEbhmSxAA;__Secure-3PSIDTS=sidts-CjIB7wV3saV3hV8wjb6dGFtm_sPA85jqqygakc5JiaOhtUVOpOdvui_cbgfIVYxsEbhmSxAA;__Secure-1PSID=g.a000rQjsn32GTZix0H63r-03LdqfckVowcTJ8U6_JUT1fYHlxRXiyJOGSjKAKUAPlwa-5yxzFwACgYKAdMSARQSFQHGX2MiiLAM-eCFGhMjZCd6mf3ZTRoVAUF8yKrZTZKjZ6SvQUq3brqb8F_-0076;__Secure-3PSID=g.a000rQjsn32GTZix0H63r-03LdqfckVowcTJ8U6_JUT1fYHlxRXi9gr2LJ-XkecjMGQfkr1DMwACgYKAeESARQSFQHGX2Mi4bADnPUkY7QDktGYjUj9txoVAUF8yKoUzentZ7aq0L28UaRG-sFP0076;HSID=AIsq6F_cOn1uoKNo-;SSID=AyxtcsCkqv-IiNqrZ;APISID=F2gPTuMK4uOoKqXg/AG6QVTAohJVvz6SI-;SAPISID=Hitzejtt91qjDkZ5/AOXwVhVtreBkZZgZa;__Secure-1PAPISID=Hitzejtt91qjDkZ5/AOXwVhVtreBkZZgZa;__Secure-3PAPISID=Hitzejtt91qjDkZ5/AOXwVhVtreBkZZgZa;LOGIN_INFO=AFmmF2swRQIgZZIQT8hUOn6VG_iTbGFVgEANCB0Pb9Wx1VdWXeKIywYCIQDa8jGmv1wLKTqo4JkuC-Xs5g6oX04IBsr9m3u-FXqZow:QUQ3MjNmd1hVNzc2UUtXM3NOdXd6YUhUNExmaHRoNG9saFB4Z1ZvaElxZVptNnFFUkRVZncwWXBGN056eU5aNnJOSms3M0tacXFNaFpKRUlETlpvY0NSanZGck9NZjk1eGljSnNJMnJxRDFvMnhPVDQ3OENlRVpLNHhwMkFmbXU4eWVRN0xUQTF3Qjh1MWl1OF9VNk5GcUl1ZjZiNHlCMXpB;YSC=scIdQOU9Yn8;SIDCC=AKEyXzWWnknJ-jyHYOzK9JvVlYR0YDeZF_DaYb-tfE6VP21M40SV8R0iZEBYhlR9cYeqLiOrww;__Secure-1PSIDCC=AKEyXzXkogrxf2rCC-QNR9uj7La_IifQ-W19LcsqMwdVouJOw-MRa0ByHVVu6s0DNlBNfBG4aw;__Secure-3PSIDCC=AKEyXzXbS5OSsrL7imtonZquLNnMKu9NN6wBjt8u0GETsIMXucvwUAIJ_QXlTcajxwvJ4b1yTg"
+  cookie:
+    "_gcl_au=1.1.1502659297.1728791448;VISITOR_INFO1_LIVE=NvNH48Rg2dE;VISITOR_PRIVACY_METADATA=CgJJThIEGgAgJA%3D%3D;PREF=repeat=NONE&volume=100&guide_collapsed=false;SID=g.a000rQjsn32GTZix0H63r-03LdqfckVowcTJ8U6_JUT1fYHlxRXizf8YjdglJfdDyAUTld-bNwACgYKAY4SARQSFQHGX2Mi04b-rfj4nxoOMKhqXSruPBoVAUF8yKq091uA0aDNjSCrhqgSMvtq0076;__Secure-1PSIDTS=sidts-CjIB7wV3saV3hV8wjb6dGFtm_sPA85jqqygakc5JiaOhtUVOpOdvui_cbgfIVYxsEbhmSxAA;__Secure-3PSIDTS=sidts-CjIB7wV3saV3hV8wjb6dGFtm_sPA85jqqygakc5JiaOhtUVOpOdvui_cbgfIVYxsEbhmSxAA;__Secure-1PSID=g.a000rQjsn32GTZix0H63r-03LdqfckVowcTJ8U6_JUT1fYHlxRXiyJOGSjKAKUAPlwa-5yxzFwACgYKAdMSARQSFQHGX2MiiLAM-eCFGhMjZCd6mf3ZTRoVAUF8yKrZTZKjZ6SvQUq3brqb8F_-0076;__Secure-3PSID=g.a000rQjsn32GTZix0H63r-03LdqfckVowcTJ8U6_JUT1fYHlxRXi9gr2LJ-XkecjMGQfkr1DMwACgYKAeESARQSFQHGX2Mi4bADnPUkY7QDktGYjUj9txoVAUF8yKoUzentZ7aq0L28UaRG-sFP0076;HSID=AIsq6F_cOn1uoKNo-;SSID=AyxtcsCkqv-IiNqrZ;APISID=F2gPTuMK4uOoKqXg/AG6QVTAohJVvz6SI-;SAPISID=Hitzejtt91qjDkZ5/AOXwVhVtreBkZZgZa;__Secure-1PAPISID=Hitzejtt91qjDkZ5/AOXwVhVtreBkZZgZa;__Secure-3PAPISID=Hitzejtt91qjDkZ5/AOXwVhVtreBkZZgZa;LOGIN_INFO=AFmmF2swRQIgZZIQT8hUOn6VG_iTbGFVgEANCB0Pb9Wx1VdWXeKIywYCIQDa8jGmv1wLKTqo4JkuC-Xs5g6oX04IBsr9m3u-FXqZow:QUQ3MjNmd1hVNzc2UUtXM3NOdXd6YUhUNExmaHRoNG9saFB4Z1ZvaElxZVptNnFFUkRVZncwWXBGN056eU5aNnJOSms3M0tacXFNaFpKRUlETlpvY0NSanZGck9NZjk1eGljSnNJMnJxRDFvMnhPVDQ3OENlRVpLNHhwMkFmbXU4eWVRN0xUQTF3Qjh1MWl1OF9VNk5GcUl1ZjZiNHlCMXpB;YSC=scIdQOU9Yn8;SIDCC=AKEyXzWWnknJ-jyHYOzK9JvVlYR0YDeZF_DaYb-tfE6VP21M40SV8R0iZEBYhlR9cYeqLiOrww;__Secure-1PSIDCC=AKEyXzXkogrxf2rCC-QNR9uj7La_IifQ-W19LcsqMwdVouJOw-MRa0ByHVVu6s0DNlBNfBG4aw;__Secure-3PSIDCC=AKEyXzXbS5OSsrL7imtonZquLNnMKu9NN6wBjt8u0GETsIMXucvwUAIJ_QXlTcajxwvJ4b1yTg",
 });
 (async () => {
   app.get("/stream/:songId", async (req, res) => {
@@ -52,8 +53,8 @@ const yt = await Innertube.create({
     const all = req.query.all;
     try {
       songId = songId.length == 11 ? songId : decrypt(req.params.songId);
-      if (VibeCache.has(songId)) {
-        return res.json(VibeCache.get(songId));
+      if (VibeCache.has(songId + all ? "all" : "notall")) {
+        return res.json(VibeCache.get(songId + all ? "all" : "notall"));
       }
       const d = await yt.music.getUpNext(songId);
       const playload = d.contents
@@ -96,7 +97,7 @@ const yt = await Innertube.create({
           ],
         }))
         .slice(!all ? 1 : 0, 20);
-      VibeCache.set(songId, playload);
+      VibeCache.set(songId, playload + all ? "all" : "notall");
       res.json(playload || []);
     } catch (error) {
       console.error(`Error getting suggestion for song: ${songId}`, error);
