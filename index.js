de7cbb3d4919e80709282fc737e61592934b30d0
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-03-25 11:57:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -241,7 +241,7 @@ async function stream(yt, songId, video, isIPhone, res) {
     type: video || isIPhone ? "video+audio" : "audio",
     quality: "best",
     format: "any",
-    client: "TV_EMBEDDED",
+    client: "YTMUSIC",
   });
 
   console.info(`Loaded audio stream for song with ID: ${songId}`);
